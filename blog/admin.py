435d37f976f7b4f8791d62a9e68f7fffedb58813
Eldorbek34 <tpgcom1@gmail.com>
2024-07-15 10:30:15 +0500

    django_imtihon

--- blog/admin.py
+++ blog/admin.py
@@ -1,43 +1,56 @@
 from django.contrib import admin
-from .models import Company, Product, Sale
-from django.core.exceptions import ValidationError
+from blog.models import Product, Company, Sale, Customer
 
+
+@admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('title', 'company', 'qty', 'price')
+    list_display = ('title', 'id', 'price', 'qty', 'company')
     fields = ('title', 'company', 'qty', 'price')
-    readonly_fields = ('company',)
 
-    def delete_model(self, request, obj):
-        if obj.qty > 0:
-            raise ValidationError("Mahsulot soni 0 dan katta bo'lsa, uni o'chirib bo'lmaydi.")
-        super().delete_model(request, obj)
-
-class CompanyAdmin(admin.ModelAdmin):
-    list_display = ('title', 'phone', 'address', 'products_count')
-
-    def delete_model(self, request, obj):
-        if any(product.qty > 0 for product in obj.products.all()):
-            raise ValidationError("Bu kompaniyada mahsulotlar mavjud, uni o'chirib bo'lmaydi.")
-        super().delete_model(request, obj)
-
-class SaleAdmin(admin.ModelAdmin):
-    list_display = ('customer_name', 'product', 'quantity', 'sale_date')
-    readonly_fields = ('customer_name', 'product', 'quantity', 'sale_date', 'sale_sum')
-    fields = ('customer_name', 'product', 'quantity')
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return ['company']
+        else:
+            return []
 
     def has_delete_permission(self, request, obj=None):
-        return False
+        if obj and obj.qty > 0:
+            return False
+        return True
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('title', 'phone', 'address', 'products_type', 'get_total_qty')
+
+    def has_delete_permission(self, request, obj=None):
+        if obj and any(product.qty > 0 for product in obj.product_set.all()):
+            return False
+        return super().has_delete_permission(request, obj)
+
+    def get_total_qty(self, obj):
+        products = Product.objects.filter(company=obj)
+        total_qty = sum(product.qty for product in products)
+        return total_qty
+
+    get_total_qty.short_description = "Mahsulot soni"
+
+
+@admin.register(Sale)
+class SaleAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'company', 'product', 'quantity_sold', 'total_amount', 'sale_date')
+    # fields = ('customer', 'product', 'quantity_sold')
+
+    def has_add_permission(self, request):
+        return True
 
     def has_change_permission(self, request, obj=None):
         return False
 
-    def save_model(self, request, obj, form, change):
-        if obj.product.qty < obj.quantity:
-            raise ValidationError("Mahsulot yetarli emas.")
-        obj.product.qty -= obj.quantity
-        obj.product.save()
-        super().save_model(request, obj, form, change)
+    def has_delete_permission(self, request, obj=None):
+        return False
 
-admin.site.register(Company, CompanyAdmin)
-admin.site.register(Product, ProductAdmin)
-admin.site.register(Sale, SaleAdmin)
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email',)
--- blog/models.py
+++ blog/models.py
@@ -1,71 +1,88 @@
-from django.db import models
 from django.core.exceptions import ValidationError
+from django.db import models
+from django.utils import timezone
+
+
 
 class Company(models.Model):
-    title = models.CharField("Kompaniya nomi", max_length=250)
-    phone = models.CharField("Tel raqami", max_length=20)
-    address = models.TextField("lakatsiya")
-
-    @property
-    def products_count(self):
-        return self.products.count()
-
-    def __str__(self):
-        return self.title
-
-    def delete(self, *args, **kwargs):
-        if any(product.qty > 0 for product in self.products.all()):
-            raise ValidationError("Ushbu kompaniyada mahsulotlar mavjud,  o'chirib bo'lmaydi.")
-        super().delete(*args, **kwargs)
+    title = models.CharField("Company title", max_length=250)
+    phone = models.CharField("Company phone", max_length=50)
+    address = models.CharField("Company address", max_length=250)
 
     class Meta:
         verbose_name_plural = "Kompaniyalar"
-        verbose_name = "kompaniya"
+        verbose_name = "Kompaniya"
 
+    def str(self):
+        return self.title
+
+    def products_type(self):
+        return self.product_set.count()
+
+    products_type.short_description = "Mahsulot turi"
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+
+    class Meta:
+        verbose_name_plural = "Xaridorlar"
+
+    def str(self):
+        return self.name
 class Product(models.Model):
     title = models.CharField("Mahsulot nomi", max_length=250)
-    price = models.IntegerField("Narxi")
-    qty = models.IntegerField("Mahsulot soni", default=0)
-    company = models.ForeignKey('Company', related_name='products', verbose_name='Kompaniya', on_delete=models.CASCADE)
+    price = models.IntegerField("Narxi",)
+    qty = models.IntegerField("Mahsulot soni",default=0)
+    company = models.ForeignKey('blog.Company', verbose_name='Kompaniya', on_delete=models.CASCADE)
 
     def clean(self):
         if self.qty < 0:
-            raise ValidationError("Kechirasiz butun son kriting")
+            raise ValidationError("Manfiy son kiritsh mumkin emas")
         if self.price < 0:
-            raise ValidationError("Kechirasiz butun son kriting")
+            raise ValidationError("Manfiy son kiritsh mumkin emas")
 
-    def __str__(self):
-        return f"{self.title} ({self.company})"
-
-    def delete(self, *args, **kwargs):
-        if self.qty > 0:
-            raise ValidationError("Mahsulot soni 0 dan katta bo'lsa, uni o'chirib bo'lmaydi.")
-        super().delete(*args, **kwargs)
+    def str(self):
+        return f"{self.title} {self.company}"
 
     class Meta:
         verbose_name_plural = "Mahsulotlar"
         verbose_name = "mahsulot"
 
+
+
 class Sale(models.Model):
-    customer_name = models.CharField("Haridorning nomi", max_length=255, default='Olim mani ismim')
-    product = models.ForeignKey(Product, verbose_name='Mahsulot', on_delete=models.PROTECT)
-    quantity = models.PositiveIntegerField("Mahsulot soni")
-    sale_date = models.DateTimeField("Sana", auto_now_add=True)
-
-    @property
-    def sale_sum(self):
-        return self.quantity * self.product.price
-
-    def __str__(self):
-        return f'{self.customer_name}ga {self.product.title} mahsuloti sotuvi'
-
-    def save(self, *args, **kwargs):
-        if self.product.qty < self.quantity:
-            raise ValidationError("Mahsulot yetarli emas.")
-        self.product.qty -= self.quantity
-        self.product.save()
-        super().save(*args, **kwargs)
+    customer = models.ForeignKey('blog.Customer', verbose_name='Xaridor', on_delete=models.CASCADE)
+    product = models.ForeignKey('blog.Product', verbose_name='Mahsulot', on_delete=models.CASCADE)
+    company = models.ForeignKey('blog.Company', verbose_name='Kompaniya' ,on_delete=models.CASCADE)
+    quantity_sold = models.IntegerField(verbose_name='Sotilgan soni')
+    sale_date = models.DateTimeField(auto_now_add=True, verbose_name='Sotilgan vaqti')
 
     class Meta:
         verbose_name_plural = "Savdolar"
-        verbose_name = "savdo"
+        verbose_name = "Savdo"
+
+    def total_amount(self):
+        return self.product.price * self.quantity_sold
+
+    total_amount.short_description = "Umumiy narxi"
+
+    def clean(self):
+        if self.quantity_sold < 0:
+            raise ValidationError('Sotilgan soni manfiy bo\'lmasligi kerak.')
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        if self.product.company != self.company:
+            raise ValidationError('Selected product does not belong to the selected company.')
+
+        if self.product.qty >= self.quantity_sold:
+            self.product.qty -= self.quantity_sold
+            self.product.save()
+            super().save(*args, **kwargs)
+        else:
+            raise ValueError('Mahsulot soni yetarli emas!')
+
+    def str(self):
+        return f'{self.customer.name} - {self.product.title} - {self.quantity_sold}'
